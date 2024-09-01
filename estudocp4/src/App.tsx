dbe64e90cb19b836193de89b94d3871a349d0a43
enricodelguerra <ricodelguerra@gmail.com>
2024-09-01 17:25:06 -0300

    Componentes criados

---
+++ estudocp4/src/App.tsx
@@ -0,0 +1,12 @@
+import Cabecalho from "./Components/Cabecalho/Cabecalho";
+import Rodape from "./Components/Rodape/Rodape";
+
+
+export default function App(){
+  return(
+    <>
+    <Cabecalho/>
+    <Rodape/>
+    </>
+  );
+}
---
+++ estudocp4/src/Components/Rodape/Rodape.tsx
@@ -0,0 +1,7 @@
+export default function Rodape(){
+    return(
+        <footer>
+            <p>Desenvolvido por <strong>Enrico Figueiredo Del Guerra</strong></p>
+        </footer>
+    );
+}
